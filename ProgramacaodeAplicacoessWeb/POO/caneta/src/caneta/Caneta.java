d3a8dd8c67ffbc87f049535c5ed79a4b8dbbc312
Erik Sidnei Corrêa <erik562@hotmail.com>
2017-02-16 21:53:52 -0200

    add Caneta

---
+++ ProgramacaodeAplicacoessWeb/POO/caneta/src/caneta/Caneta.java
@@ -0,0 +1,25 @@
+
+package caneta;
+
+
+public class Caneta {
+    String modelo;
+    String cor;
+    double ponta;
+    int carga;
+    boolean tampada;
+    public void rabiscar (){
+        if (tampada){
+            System.out.println("Erro!");
+            
+        }else {
+            System.out.println("Rabiscando");
+        }
+    }
+    public void tampar(){
+        tampada = true;
+    }
+    public void destampar(){
+        tampada = false;
+    }
+}
---
+++ ProgramacaodeAplicacoessWeb/POO/caneta/src/caneta/testaCaneta.java
@@ -0,0 +1,19 @@
+
+package caneta;
+
+
+public class testaCaneta {
+ public static void main (String [] args){
+     Caneta c1 = new Caneta();
+     c1.cor = "Azul";
+     c1.ponta = 0.5;
+     c1.tampada = false;
+     c1.rabiscar();
+     
+     Caneta c2 = new Caneta();
+     c2.cor = "vermelha";
+     c2.ponta = 1.0;
+     c2.tampada = false;
+     c2.tampar();
+ }    
+}
